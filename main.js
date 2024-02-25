648f8688ff658739dd107e146d9747464e7ef7d7
Muhammadxusan08 <fotbol100k@gmail.com>
2024-02-25 09:05:32 +0300

    resposive section-2

--- main.js
+++ main.js
@@ -4,11 +4,26 @@ let sectionn = document.querySelector('.section')
 let headerr = document.querySelector('.header')
 
 
-
 sectionn.addEventListener('click' , function(){
 sectionn.style.display = 'none'
 })
 
 headerHamburger.addEventListener('click' , function(){
     sectionn.style.display = 'block'
+})
+
+
+let section2Link = document.querySelectorAll('.section-2-link')
+let sectionnh = document.querySelectorAll(".section-2-card-logotip")
+section2Link.forEach(function(item , index){
+item.addEventListener('click' , function(){
+    section2Link.forEach(function(element){
+        element.classList.remove('active')
+    })
+    item.classList.add("active")
+    sectionnh.forEach(function(element){
+        element.classList.remove('active')
+    })
+    sectionnh[index].classList.add('active')
+})
 })
