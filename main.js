266009c2d7638d54851c42d9bae4ed986e813b7a
Muhammadxusan08 <fotbol100k@gmail.com>
2024-02-27 18:17:46 +0300

    push

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ let sectionImg = document.querySelector('.section-img-2')
 let headerHamburger = document.querySelector('.header-hamburger')
 let sectionn = document.querySelector('.section')
 let headerr = document.querySelector('.header')
+let body = document.querySelector('body')
 
 
 sectionn.addEventListener('click' , function(){
 sectionn.style.display = 'none'
+
 })
 
 headerHamburger.addEventListener('click' , function(){
     sectionn.style.display = 'block'
+    body.classList = 'bgsg'
 })
 
 
